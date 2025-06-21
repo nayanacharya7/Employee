f15cb88267e54dd3a363bcfc5b559ed314d6b168
Nayan kumar <nayan7acharya@gmail.com>
2025-06-21 16:47:08 +0530

    Update EmployeeController.java
    Some rest APIs

--- src/main/java/com/example/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -114,4 +114,172 @@ public class EmployeeController {
 	        return new ResponseEntity<>("Error updating employee: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	    }
 	}
+/*	@RequestMapping(value = "/getELSSCalculatorResult", method = RequestMethod.POST)
+		public void getELSSCalculatorResult(HttpServletRequest request, HttpServletResponse response) throws IOException
+		{
+			PrintWriter writer = null;		
+			Gson gson = null;
+			String ipAddr = null;
+			String origin = null;
+			MutualFundApiAndIpValidityResponse mutualFundApiAndIpValidityResponse = null;
+			ApiCalculatorELSS result = null;
+			String apirequest = "",apiresponse = "";
+			Date d1;
+			String client_name = "";
+			
+			try 
+			{	
+				d1 = new Date();
+				writer = response.getWriter();
+				gson = new GsonBuilder().registerTypeAdapter(Date.class, new DateSerializer()).create();
+				response.setContentType("text/html");
+				ipAddr = CommonUtils.getIpAddr(request);
+				if(ipAddr == null){ipAddr="";}
+				origin = request.getHeader("Origin");
+				if(origin == null){origin="";}
+				result = new ApiCalculatorELSS();
+				
+				String amount = request.getParameter("investment_amount");
+		     		String interest_rate = request.getParameter("interest_rate");
+	            		String period = request.getParameter("period");
+	            		String type = request.getParameter("type");
+	            		String key = request.getParameter("key");
+	            
+	            String[][] param_and_pattern = {{amount,"Amount"},{interest_rate,"SafeString"},{period,"SafeString"},{type,"SafeString"}};
+	            
+	            int invalid_index_para = ESAPIValidator.isValidParameters(param_and_pattern); 
+	            if(invalid_index_para > -1) 
+	            {
+	            	String[] parameters_name = {"investment_amount","interest rate","period","type"};
+	            	String validationErrorMsg = "Invalid "+parameters_name[invalid_index_para];
+	            	result.setStatus(StatusMessage.FailureCode);
+	            	result.setStatus_msg(StatusMessage.FailureMessage);
+	            	result.setMsg(validationErrorMsg);
+	            	apiresponse = gson.toJson(result);                                       
+	            	writer.print(apiresponse);/*Esapi FailureResponse*/
+	    		writer.close();
+	            	return;
+	            }
+	            
+		    if(amount == null || StringHelper.isEmpty(amount)){amount = "";}
+	            if(interest_rate == null || StringHelper.isEmpty(interest_rate)){interest_rate = "";}
+	            if(period == null || StringHelper.isEmpty(period)){period = "";}
+	            if(type == null || StringHelper.isEmpty(type)){type = "SIP";}
+	            if(key == null || StringHelper.isEmpty(key)){key = "";}
+	            
+	            
+	            amount = amount.trim();
+	            interest_rate = interest_rate.trim();
+	            period = period.trim();
+	            type = type.trim();
+				key = key.trim();
+				
+	            apirequest += "/calc/getELSSCalculatorResult?";
+	            apirequest += "origin="+origin+"&";
+	            apirequest += "ipAddr="+ipAddr+"&";
+	            apirequest += "key="+key+"&";
+	            apirequest += "investment_amount="+amount+"&";
+	            apirequest += "interest_rate="+interest_rate+"&";
+	            apirequest += "period="+period;
+	            apirequest += "type="+type;
+	            
+	            String api = "/calc/getELSSCalculatorResult";
+	            mutualFundApiAndIpValidityResponse = apiDao.isApiKeyAndIpValid(key, ipAddr, origin, api);
+	            client_name = mutualFundApiAndIpValidityResponse.getClient_name();
+	            
+	            String apirequest1 = CommonUtils.formatApiRequest(apirequest);  
+	            Integer id = apiDao.saveApiLogRequest(apirequest1, api, client_name, ipAddr, origin);
+	            
+	            if(mutualFundApiAndIpValidityResponse!=null && mutualFundApiAndIpValidityResponse.isValid())
+	            {
+	            	if(!NumberUtils.isParsable(period)) {
+	            		result.setStatus(StatusMessage.FailureCode);
+	                	result.setStatus_msg(StatusMessage.FailureMessage);
+	                	result.setMsg("Invalid Period");	
+	                	
+	                	apiresponse = gson.toJson(result);
+	                	apiDao.updateApiResponseById(id, apiresponse.length()>100?apiresponse.substring(0, 100):apiresponse, d1);                                       
+	                	writer.print(apiresponse);
+	        		    writer.close();
+	                	return;
+	            	}
+	            	
+	            	if(!NumberUtils.isParsable(amount) || Integer.parseInt(amount) < 1) {
+	            		result.setStatus(StatusMessage.FailureCode);
+	                	result.setStatus_msg(StatusMessage.FailureMessage);
+	                	result.setMsg("Invalid Amount");	  
+	                	
+	                	apiresponse = gson.toJson(result);
+	                	apiDao.updateApiResponseById(id, apiresponse.length()>100?apiresponse.substring(0, 100):apiresponse, d1);                                       
+	                	writer.print(apiresponse);
+	        		    writer.close();
+	                	return;
+	            	}
+	            	
+	            	if(!NumberUtils.isParsable(interest_rate)) {
+	            		result.setStatus(StatusMessage.FailureCode);
+	                	result.setStatus_msg(StatusMessage.FailureMessage);
+	                	result.setMsg("Invalid Interest Rate");	 
+	                	
+	                	apiresponse = gson.toJson(result);
+	                	apiDao.updateApiResponseById(id, apiresponse.length()>100?apiresponse.substring(0, 100):apiresponse, d1);                                       
+	                	writer.print(apiresponse);
+	        		    writer.close();
+	                	return;
+	            	}
+	            	
+	            	long investAmount = Long.parseLong(amount);
+	            	double interestRate = Double.parseDouble(interest_rate);
+	            	int years = Integer.parseInt(period); // in years
+	            	long investedAmount = 0;
+	            	long growthValue = 0;
+	            	long maturityAmount = 0;
+
+	            	if (type.equalsIgnoreCase("SIP")) {
+	            		//convert year to month for SIP
+	            	    int totalMonths = years * 12;
+	            	    Double pv = 0.0;
+	            	    Double finalAmount = TrackerUtils.getFutureValue(interestRate, totalMonths, investAmount * -1.0, pv, 1);
+	            	    maturityAmount = Math.round(finalAmount);
+	            	    investedAmount = investAmount * totalMonths;
+	            	    growthValue = maturityAmount - investedAmount;
+	            	}
+
+	            	if (type.equalsIgnoreCase("Lumpsum")) {
+	            		
+	            	    double value1 = 1 + (interestRate / 100);
+	            	    double value2 = Math.pow(value1, years);
+	            	    double futureValue = investAmount * value2;
+	            	    maturityAmount = Math.round(futureValue);
+	            	    investedAmount = investAmount;
+	            	    growthValue = maturityAmount - investedAmount;
+	            	}
+	            	
+	            	result.setStatus(StatusMessage.SuccessCode);
+	            	result.setStatus_msg(StatusMessage.SuccessMessage);
+	            	result.setMsg(StatusMessage.SuccessMessage);
+	            	result.setType(type);
+	            	result.setInvested_amount(investedAmount);
+	            	result.setGrowth_value(growthValue);
+	            	result.setMaturity_amount(maturityAmount);
+	            }
+	            else
+	            {
+	            	result.setStatus(StatusMessage.FailureCode);
+	            	result.setStatus_msg(StatusMessage.FailureMessage);
+	            	result.setMsg(mutualFundApiAndIpValidityResponse.getValid_msg());
+	            }
+	            apiresponse = gson.toJson(result);
+	            apiDao.updateApiResponseById(id, apiresponse.length()>100?apiresponse.substring(0, 100):apiresponse, d1);
+	    		writer.print(apiresponse);
+			    writer.close();
+			} 
+			catch (Exception ex) 
+			{
+				ex.printStackTrace();
+				writer.print(CommonUtils.getFailureResponse());
+				writer.close();
+			}
+		}
+*/
 }
